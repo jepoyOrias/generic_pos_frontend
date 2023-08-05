c1d48b9f79bec93577f6c3178860cf997d53de1f
unknown <jeffreyorias26@gmail.com>
2023-08-05 14:49:37 +0800

    server upload

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import {BootstrapVue3} from 'bootstrap-vue-3'
+import Vue3Lottie from "vue3-lottie";
 
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/css/custom.scss'
@@ -16,5 +17,6 @@ const pinia = createPinia();
  createApp(App).use(pinia)
               .use(router)
               .use(BootstrapVue3)
+              .use(Vue3Lottie)
               .mount('#app')
 
--- src/store/auth/AuthStore.js
+++ src/store/auth/AuthStore.js
@@ -1,4 +1,12 @@
 import { defineStore } from "pinia";
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: 'https://go-jep.000webhostapp.com/api/'
+});
+
+axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
+axiosInstance.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 export const useAuthStore = defineStore('AuthStore', {
     state: ()=>({
@@ -11,8 +19,15 @@ export const useAuthStore = defineStore('AuthStore', {
         isAuthenticated: (state) => state.isAuthenticated,
     },
     actions: {
-      login(){
-            this.isAuthenticated =  true;
+       login(email, password){
+               axiosInstance.post('login', {
+                  email: email,
+                  password: password,
+
+              }).then(response => console.log(response))
+              .catch(error => console.log(error))
+            
+            
         },
       logout(){
             this.isAuthenticated =  false;
